11ecfac07674be18bcdfabbc8d8e46073875ee0a
unknown <shanu.divya123@gmail.com>
2022-01-10 17:21:51 +0530

    master

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'; 
+import Navbar from './components/Navbar';
+import './App.css';
+import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './components/pages/Home';
+
+
+
+function App() {
+  return (
+    <>
+    <Router>
+      <Navbar />
+      <Routes>
+           <Route path='/' exact element={<Home />} ></Route>
+      </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/CardItems.js
@@ -0,0 +1,12 @@
+import react from "react";
+import {Link} from react-router-dom; 
+
+function CardItems() {
+    return(
+       <div>
+
+       </div>
+        
+    )
+}
+export default CardItems;
