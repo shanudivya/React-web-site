73d211b989bdd21d4d636a6821f0b8227449b942
unknown <shanu.divya123@gmail.com>
2022-01-17 12:35:03 +0530

    master

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import Navbar from './components/Navbar';
 import './App.css';
 import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './components/pages/Home';
-
-
+import Services from './components/pages/Services';
+import Product from './components/pages/Product';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
@@ -13,6 +14,9 @@ function App() {
       <Navbar />
       <Routes>
            <Route path='/' exact element={<Home />} ></Route>
+           <Route path='/services' element={Services} /> 
+           <Route path='/product' element={Product} /> 
+           <Route path='/sign-up' element={SignUp} /> 
       </Routes>
       </Router>
     </>
--- src/components/CardItems.js
+++ src/components/CardItems.js
@@ -1,27 +1,26 @@
-import React from "react";
-import {Link} from "react-router-dom"; 
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-function CardItems(props) {
-    return(
-       <>
-        <li className="cards_item">
-        <Link  className="cards__item__link" to={props.path}>
-            <figure className="cards__item__pic-wrap" data-category={props.label}>
-                <img 
-                src="props.src" 
-                alt="Travel Image"
-                className="cards__item__img" 
-                />
-            </figure>
-            <div className="cards__item__info">
-                <h5 className="cards__item__text">{props.text}</h5> 
-
-            </div>
-        
+function CardItem(props) {
+  return (
+    <>
+      <li className='cards__item'>
+        <Link className='cards__item__link' to={props.path}>
+          <figure className='cards__item__pic-wrap' data-category={props.label}>
+            <img
+              className='cards__item__img'
+              src={props.src}
+              alt='travel image'
+               />
+               
+          </figure>
+          <div className='cards__item__info'>
+            <h5 className='cards__item__text'>{props.text}</h5>
+          </div>
         </Link>
-        </li>
-       </>
-        
-    );
+      </li>
+    </>
+  );
 }
-export default CardItems;
+
+export default CardItem;
